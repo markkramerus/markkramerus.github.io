3cd95fde1f27db8208c55071b18f836d7b242651
markkramerus <mkramer@mitre.org>
2020-04-13 09:32:28 -0400

    Update baseUrl to ''

--- 704d4d55.49cd217b.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{102:function(o){o.exports=JSON.parse('{"id":"c-19-new-hydroxy","title":"New user of hydroxychloroquine","description":"**Cohort name**: New user of hydroxychloroquine\\r","source":"@site/..\\\\docs\\\\c-19-new-hydroxy.md","permalink":"/docs/c-19-new-hydroxy"}')}}]);
---
+++ 7153c6fa.b0f3fd1c.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{101:function(i){i.exports=JSON.parse('{"id":"overview","title":"Overview","description":"## Clinical Data Standardization for COVID-19: Call to Action\\r","source":"@site/..\\\\docs\\\\overview.md","permalink":"docs/overview"}')}}]);
